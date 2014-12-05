5bf9cadacc04016b2b420e73e7ac260fa558b767
vedraiyani <vedraiyani@gmail.com>
2014-12-05 12:15:40 +0530

    testmyserver

--- www/config.js
+++ www/config.js
@@ -1,5 +1,6 @@
 var config = {
-    servicerooturl: "http://live.issousa.org/kirtanbhakti/",
+    servicerooturl:"http://192.168.137.1/kbs/",
+    //servicerooturl: "http://localhost:8080/htdocs/kbs/",
     fbAppId: '381831755318109',
     AppInfo: {
         version: "1.0.0",
