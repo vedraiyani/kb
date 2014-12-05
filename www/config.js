adc39255be3d1e704441d0f353b2160c4078a794
vedraiyani <vedraiyani@gmail.com>
2014-12-05 11:13:10 +0530

    5122014 submission

---
+++ www/config.js
@@ -0,0 +1,9 @@
+var config = {
+    servicerooturl: "http://live.issousa.org/kirtanbhakti/",
+    fbAppId: '381831755318109',
+    AppInfo: {
+        version: "1.0.0",
+        author: "test",
+        description: ""
+    }
+}
