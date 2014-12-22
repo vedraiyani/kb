f80dd078bee22f7848df6c01cd95b7a5ded07bda
vedraiyani <vedraiyani@gmail.com>
2014-12-22 16:38:58 +0530

    updating server

--- www/config.js
+++ www/config.js
@@ -1,7 +1,7 @@
 var config = {
     //servicerooturl:"http://192.168.137.1/kbs/",
     //servicerooturl: "http://localhost:8080/htdocs/kbs/",
-    servicerooturl:"http://issousa.co/kirtanbhakti/",
+    servicerooturl:"http://live.issousa.org/kirtanbhakti/",
     fbAppId: '381831755318109',
     fbMessage: 'Kirtan Bhakti',
     AppInfo: {
