00f00aa91787fea9adab9cc002cd5e5ada707adc
Dipeshkumar Patel <drpatel1111@yahoo.com>
2015-02-09 11:34:41 -0500

    Update config.js
    Ver change svc url

--- www/config.js
+++ www/config.js
@@ -2,11 +2,11 @@ var config = {
     //servicerooturl:"http://192.168.137.1/kbs/",
     //servicerooturl: "http://localhost:8080/htdocs/kbs/",
     //servicerooturl:"http://live.issousa.org/kirtanbhakti/",
-	servicerooturl:"http://issousa.co/kirtanbhakti/",
+	servicerooturl:"http://live.issousa.org/kirtanbhakti/",
     fbAppId: '731387763597618',
     fbMessage: 'Kirtan Bhakti',
     AppInfo: {
-        version: "1.0.2",
+        version: "1.0.3",
         author: "vedraiyani",
         description: "http://www.qcyclone.com"
     }
